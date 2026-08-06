ffd60b67ac91b15c9195c67bbe1873b87e5807a7
MaiaT2408 <joaomarcossoaresmaia@hotmail.com>
2026-08-05 21:11:40 -0300

    Add perguntas.js with quiz questions and answers

---
+++ perguntas.js
@@ -0,0 +1,14 @@
+const perguntas = [
+  {
+    pergunta: "Quanto é 15²?",
+    respostas: ["225"]
+  },
+  {
+    pergunta: "Capital da Austrália",
+    respostas: ["camberra", "canberra"]
+  },
+  {
+    pergunta: "Qual é a raiz quadrada de 324?",
+    respostas: ["18"]
+  }
+];
